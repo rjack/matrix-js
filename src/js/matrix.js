40dafac6abd16d3a314135d436f3ae921846e6f9
Giacomo Ritucci <giacomo.ritucci@email.it>
2010-07-26 00:24:42 +0200

    `Matrix.sum' and test

---
+++ src/js/matrix.js
@@ -0,0 +1,23 @@
+/*jslint white: true, browser: true, devel: true, onevar: true, undef: true, nomen: true, eqeqeq: true, plusplus: false, bitwise: true, regexp: true, newcap: true, immed: true */
+
+this.Matrix = (function ()
+{
+	var sum = function (u, v)
+		{
+			var i, result = [];
+
+			if (v.length !== u.length) {
+				throw "Lengths don't match";
+			}
+
+			for (i = 0; i < u.length; i++) {
+				result[i] = u[i] + v[i];
+			}
+
+			return result;
+		};
+
+	return {
+		sum: sum
+	};
+}());
